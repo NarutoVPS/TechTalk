e861324517961503b42611d784c4c6cfb5ef4379
Ved Prakash Singh <singhvedprakash075@gmail.com>
2020-10-06 14:03:45 +0545

    fix

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,13 +3,13 @@ import React from "react";
 const Header = () => {
 	return (
 		<div className="ui three item stackable tabs menu">
-			<a class="item active" data-tab="Home">
+			<a className="item active" data-tab="Home">
 				Home
 			</a>
-			<a class="item" data-tab="Test 2">
+			<a className="item" data-tab="Test 2">
 				Test 2
 			</a>
-			<a class="item" data-tab="Test 3">
+			<a className="item" data-tab="Test 3">
 				Test 3
 			</a>
 		</div>
