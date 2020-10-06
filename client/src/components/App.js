b542f87f60842444a23d07c4ab71d8775967b955
Ved Prakash Singh <singhvedprakash075@gmail.com>
2020-10-06 14:03:40 +0545

    Footer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
 	return (
 		<div className="ui container">
 			<Header />
+			<Footer />
 		</div>
 	);
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div className="ui horizontal small divided link list">
+			<a
+				className="item"
+				href="http://github.com/NarutoVPS/"
+				target="_blank"
+			>
+				Created by Ved Prakash Singh
+			</a>
+		</div>
+	);
+};
+
+export default Footer;
