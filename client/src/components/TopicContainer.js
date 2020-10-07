1e43e50d527386df5305101a25de8bb03c80aa2a
Ved Prakash Singh <singhvedprakash075@gmail.com>
2020-10-07 15:55:10 +0545

    fix

--- client/src/components/TopicContainer.js
+++ client/src/components/TopicContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import PostHeader from "./PostHeader";
+import PostTitle from "./PostTitle";
 
 const TopicContainer = (props) => {
 	return (
@@ -9,9 +9,9 @@ const TopicContainer = (props) => {
 				<div className="active item">{props.topic}</div>
 			</div>
 			<div className="ui bottom attached active tab segment">
-				<PostHeader postTitle="This is a Post" />
-				<PostHeader postTitle="This is a also a Post" />
-				<PostHeader postTitle="You guessed it, a POST!" />
+				<PostTitle postTitle="This is a Post" />
+				<PostTitle postTitle="This is a also a Post" />
+				<PostTitle postTitle="You guessed it, a POST!" />
 			</div>
 		</React.Fragment>
 	);
