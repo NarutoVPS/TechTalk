de5c221425c3a9d6772a0a1d58d1e736620f7e1b
Ved Prakash Singh <singhvedprakash075@gmail.com>
2020-10-10 00:20:51 +0545

    fix: incorrect active tab

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+	const [activeTab, setActiveTab] = useState(window.location.pathname);
+
+	const onClickHandler = (e) => {
+		setActiveTab(window.location.pathname);
+	};
+
 	return (
-		<div className="ui three item unstackable tabs menu">
-			<a className="item active" data-tab="Home">
+		<div
+			onClick={onClickHandler}
+			className="ui three item unstackable tabs menu"
+		>
+			<Link
+				to="/"
+				className={`item ${activeTab == "/" ? "active" : ""}`}
+				data-tab="Home"
+			>
 				Home
-			</a>
-			<a className="item" data-tab="About">
+			</Link>
+			<Link
+				to="/about"
+				className={`item ${activeTab == "/about" ? "active" : ""}`}
+				data-tab="About"
+			>
 				About
-			</a>
-			<a className="item" data-tab="Contact">
+			</Link>
+			<Link
+				to="/contact"
+				className={`item ${activeTab == "/contact" ? "active" : ""}`}
+				data-tab="Contact"
+			>
 				Contact
-			</a>
+			</Link>
 		</div>
 	);
 };
