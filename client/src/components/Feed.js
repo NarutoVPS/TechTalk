1ed11914446d655bffc43810e92ee75ceeabba0d
Ved Prakash Singh <singhvedprakash075@gmail.com>
2020-10-09 14:25:26 +0545

    fix

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -2,19 +2,19 @@ import React from "react";
 
 const Feed = () => {
 	return (
-		<div class="ui feed">
-			<div class="event">
-				<div class="label">
+		<div className="ui feed">
+			<div className="event">
+				<div className="label">
 					<img src="https://semantic-ui.com/images/avatar/small/elliot.jpg" />
 				</div>
-				<div class="content">
-					<div class="summary">
-						<a class="user">Elliot Fu</a> added you as a friend
-						<div class="date">1 Hour Ago</div>
+				<div className="content">
+					<div className="summary">
+						<a className="user">Elliot Fu</a> added you as a friend
+						<div className="date">1 Hour Ago</div>
 					</div>
-					<div class="meta">
-						<a class="like">
-							<i class="like icon"></i> 4 Likes
+					<div className="meta">
+						<a className="like">
+							<i className="like icon"></i> 4 Likes
 						</a>
 					</div>
 				</div>
