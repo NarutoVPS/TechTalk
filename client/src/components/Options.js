cd485b06c5266170d817088901101aa0c80003d5
Ved Prakash Singh <singhvedprakash075@gmail.com>
2020-10-11 23:55:10 +0545

    fix

--- client/src/components/Options.js
+++ client/src/components/Options.js
@@ -2,22 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Options = ({ showReply, isLoggedIn }) => {
-	const checkAccess = () => {
-		if (!isLoggedIn) alert("Please Log In");
-	};
-
 	if (!showReply) {
 		return (
-			<Link to="/new" className="ui right floated green button">
+			<Link
+				to="/new"
+				className={`ui right floated green button ${
+					window.gapi.auth2.getAuthInstance().isSignedIn.get()
+						? ""
+						: "disabled"
+				}`}
+			>
 				Create a Post
 			</Link>
 		);
 	}
 
 	return (
-		<div onCLick={checkAccess}>
-			<span className="ui right floated primary button">Reply</span>
-			<Link to="/new" className="ui right floated green button">
+		<div>
+			<span
+				className={`ui right floated primary button ${
+					window.gapi.auth2.getAuthInstance().isSignedIn.get()
+						? ""
+						: "disabled"
+				}`}
+			>
+				Reply
+			</span>
+			<Link
+				to="/new"
+				className={`ui right floated green button ${
+					window.gapi.auth2.getAuthInstance().isSignedIn.get()
+						? ""
+						: "disabled"
+				}`}
+			>
 				Create a Post
 			</Link>
 		</div>
