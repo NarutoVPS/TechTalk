c2ae7290c10a4310ab08d7b59e45a9e76cd65e6b
Ved Prakash Singh <singhvedprakash075@gmail.com>
2020-10-17 22:36:00 +0545

    reply comment

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -11,6 +11,49 @@ const Feed = ({ postId, replys = [] }) => {
 	return (
 		<div className="ui feed">
 			{replys.map((reply) => {
+				if (reply.isCommentReply) {
+					const index = reply.replyIndex;
+					return (
+						<div className="event">
+							<div className="label">
+								<Link to={"/member/" + reply.email}>
+									<img src={reply.pic} />
+								</Link>
+							</div>
+							<div className="content">
+								<Link to={"/member/" + reply.email}>
+									<span className="summary">
+										<a>{reply.name}</a>
+									</span>
+								</Link>
+
+								<span>
+									<ReplyBtn
+										index={++replyIndex}
+										postId={postId}
+									/>
+								</span>
+								<div className="extra text">
+									{htmlParser(reply.body)}
+								</div>
+
+								<div className="repliedTo">
+									<div className="content">
+										<Link to={"/member/" + reply.email}>
+											<span className="summary">
+												<a>{replys[index].name}</a>
+											</span>
+										</Link>
+
+										<div className="extra text">
+											{htmlParser(replys[index].body)}
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					);
+				}
 				return (
 					<div className="event">
 						<div className="label">
